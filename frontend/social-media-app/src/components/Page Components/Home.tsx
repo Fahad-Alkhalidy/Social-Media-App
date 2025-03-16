b98e0529de4348545d101687ae5d2827ec448bae
Fahad-Alkhalidy <fahadkhalidy700@gmail.com>
2025-03-16 21:59:03 +0300

    Added friends posts on the home page functionality

--- frontend/social-media-app/src/components/Page Components/Home.tsx
+++ frontend/social-media-app/src/components/Page Components/Home.tsx
@@ -4,9 +4,25 @@ import { IUserType } from "../../Typescript Types/userType";
 import UserInfoBox from "../Functionality Component/userInfoBox";
 //import Button from "../Functionality Component/Button";
 import PagesDrawer from "../Functionality Component/Drawer";
+import { IPost } from "../../Typescript Types/postType";
+import Post from "../Functionality Component/Profile Page Components/Post";
 
 const Home: React.FC = () => {
   const [fetchedUsers, setFetchedUsers] = useState<[{ IUserType }]>();
+  const [fetchFriendPosts, setFetchFriendPosts] = useState<IPost[]>();
+  useEffect(() => {
+    const fetchFriendPosts = async () => {
+      try {
+        const response = await fetch("/api/v1/posts/FriendsPosts");
+        const result = await response.json();
+        if (response.ok) setFetchFriendPosts(result.data.friendPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFriendPosts();
+  }, []);
+  // console.log(fetchFriendPosts);
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -75,14 +91,39 @@ const Home: React.FC = () => {
         </div>
       </div>
       <div className="flex w-full flex-col lg:flex-row h-dvh">
-        <div className="card bg-base-300 rounded-box flex flex-col flex-start overflow-y-scroll max-h-screen align-top">
-          {fetchedUsers?.map((user: IUserType) => (
+        <div className="card bg-base-300 rounded-box flex flex-col flex-start overflow-y-scroll min-h-screen align-top">
+          {fetchedUsers?.map((user) => (
             <UserInfoBox User={user} displayFollowButtonAndBio={true} />
           ))}
         </div>
-        <div className="divider lg:divider-horizontal">OR</div>
-        <div className="card bg-base-300 rounded-box grid h-32 grow place-items-center">
-          content
+        <div className="divider lg:divider-horizontal"></div>
+        <div className="card bg-base-300 rounded-box flex justify-between grow place-items-center min-h-screen">
+          {/* name of each tab group should be unique */}
+          <div className="tabs tabs-border min-h-screen">
+            <div className="flex flex-col justify-center align-middle">
+              <input
+                type="radio"
+                name="my_tabs_2"
+                className="tab"
+                aria-label="Friends' Posts"
+              />
+              <div className="tab-content border-base-300 bg-base-100 p-10 max-h-150 overflow-y-scroll w-130">
+                {fetchFriendPosts?.map((post) => (
+                  <Post Post={post}></Post>
+                ))}
+              </div>
+              <input
+                type="radio"
+                name="my_tabs_2"
+                className="tab"
+                aria-label="Explore"
+                defaultChecked
+              />
+              <div className="tab-content border-base-300 bg-base-100 p-10 w-130">
+                Tab content 2
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
